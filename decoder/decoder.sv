4dda2f7703a103ea5c980a69fe6e6eb137a4748d
[ Matteo Cericola] <m.cericola06@gmail.com>
2025-11-04 12:25:00 -0500

    Finished the testbench and the module for the decoder

--- decoder/decoder.sv
+++ decoder/decoder.sv
@@ -7,15 +7,13 @@ module decoder # (
     parameter OUTPUT_WIDTH = 8, 
     parameter INPUT_WIDTH = $clog2(OUTPUT_WIDTH)
   )(
-    input  [OUTPUT_WIDTH-1:0] decoderIn,
-    output [INPUT_WIDTH-1:0]  decoderOut
+    input logic [INPUT_WIDTH-1:0] decoderIn,
+    output logic [OUTPUT_WIDTH-1:0]  decoderOut
   );
 
     always_comb begin
-        for (int i = 0; i < OUTPUT_WIDTH; i++) begin
-            if (i == decoderIn) decoderOut[i] = 1'b1;
-            else                decoderOut[i] = 1'b0;
-        end
+        decoderOut = 0;
+        decoderOut[decoderIn] = 1'b1;
     end
 
 endmodule   
--- decoder/testDecoder.sv
+++ decoder/testDecoder.sv
@@ -1,19 +1,30 @@
-module testDecoder; 
-    logic [2:0] decoderIn;
-    logic [7:0] decoderOut;
+`timescale 1ns/1ps
 
-    dut(decoder(decoderIn, decoderIn));
+
+module testDecoder; 
+    localparam OUTPUT_WIDTH = 8;
+    localparam INPUT_WIDTH = $clog2(OUTPUT_WIDTH);
+    logic [INPUT_WIDTH-1:0] decoderIn;
+    logic [OUTPUT_WIDTH-1:0] decoderOut;
+
+    decoder #(OUTPUT_WIDTH, INPUT_WIDTH) dut (decoderIn, decoderOut);
+
+    logic [INPUT_WIDTH-1:0] outputSum;
 
     initial begin
         decoderIn = 3'b0;
-        $monitor("time %b, decoderIn %b, decoderOut %b", $time, decoderIn, decoderOut)
-        for (i = 3'b0; i < 4'b8; i = i + 1) begin
+        $monitor("decoderIn %b, decoderOut %b", decoderIn, decoderOut);
+        for (int i = 'b0; i < OUTPUT_WIDTH; i += 1) begin
             decoderIn = i;
-            #4
-            if (decoderOut != {decoderIn[2] + decoderIn[1] + decoderOut[0]}) begin
-                $display("Whoops decoderIn = %b, decoderOut = %b", decoderIn, decoderOut);
+            outputSum = 3'b0;
+            #4;
+            for (int j = 'b0; j < OUTPUT_WIDTH; j += 1)
+                if (decoderOut[j] == 1'b1) outputSum += j;
+            if (decoderIn != outputSum) begin
+                $display("Whoops decoderIn = %b, decoderOut = %b, outputSum = %b", decoderIn, decoderOut, outputSum);
             end
         end
-        $finish
+        #4;
+        $finish;
     end
 endmodule
